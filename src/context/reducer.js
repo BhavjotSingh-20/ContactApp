e216e71fd605bef69984d767a0cfbc9b3cf6a1db
Bhavjot Singh <bhavjot794@gmail.com>
2021-03-01 00:34:16 +0530

    Context action reducer completed

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -8,3 +8,28 @@ import {
 } from "./action.types";
 
 //TODO: use switch case
+
+export default (state,action) => {
+switch (action.type) {
+  case SET_CONTACT:
+    return action.payload == null ? {...state,contacts:[]} : {...state,contacts:action.payload}
+    case SET_LOADING:
+      return {...state,isLoading:action.payload}
+
+    case CONTACT_TO_UPDATE:
+      return {
+        ...state,
+        contactToUpdate:action.payload,
+        contactToUpdateKey: action.Key
+      }  
+
+      case SET_SINGLE_CONTACT:
+         return {
+           ...state,
+           contact : action.payload
+         }
+
+  default:
+    return state
+}
+}
